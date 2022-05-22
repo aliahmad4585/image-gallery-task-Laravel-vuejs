36844bfebedd3604e61c45dc0eb50522ced9e3e8
Ali Ahmad <aliahmad4585256@gmail.com>
2022-05-22 05:59:02 +0500

    a

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,14 +10,14 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="https://still-shore-35289.herokuapp.com/js/app.js" defer></script>
+    <script src="{{ asset('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
-    <link href="https://still-shore-35289.herokuapp.com/css/app.css" rel="stylesheet">
+    <link href="//still-shore-35289.herokuapp.com/css/app.css" rel="stylesheet">
 </head>
 <body>
     <div id="app">
